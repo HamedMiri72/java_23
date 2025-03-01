1e0f06911adb8e2fd4240970552c7dddee797632
HamedMiri72 <hamedmiri72@gmail.com>
2025-03-01 12:19:12 +0000

    first commitment

---
+++ src/class1/com/Main.java
@@ -0,0 +1,26 @@
+package class1.com;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        //I Started working with java 23
+
+        /*
+        this
+        is
+        a
+        multi-line
+        comment
+         */
+        System.out.print("hello world!\n");
+        System.out.println("i like pitzza its really good!");
+        System.out.print("buy me a pitzza!");
+
+
+        System.out.println("finished this class");
+
+
+
+    }
+}
