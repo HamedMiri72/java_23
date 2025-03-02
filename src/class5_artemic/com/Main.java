ef0b6d20fd06db48a36f9e332ae28c89b7cbb16c
HamedMiri72 <hamedmiri72@gmail.com>
2025-03-02 23:15:18 +0000

    class5_arthemic

---
+++ src/class5_artemic/com/Main.java
@@ -0,0 +1,36 @@
+package class5_artemic.com;
+
+public class Main {
+    public static void main(String[] args){
+
+
+        int x = 10;
+        int y = 2;
+
+        int z;
+
+
+        z = x + y;
+        //z = x - y;
+        // % reminder of any division
+
+        System.out.println(z);
+
+        //augmented assignment operators
+
+        int a = 10;
+        int b = 3;
+
+        //a = a + y;
+        a += b;
+        a++;
+        a--;
+
+
+        // order of operations (pemdas) => prentecis exponent multiply devision addition substraction
+
+        double result = 3 + 4 * (7 - 5) / 2;
+        System.out.println(result);
+
+    }
+}
