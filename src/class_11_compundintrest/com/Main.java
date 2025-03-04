b12ed671a2ff17f7154f185bdc2facaf07285eaf
HamedMiri72 <hamedmiri72@gmail.com>
2025-03-04 12:17:57 +0000

    class-11 compounent claculator

--- src/class_11_compundintrest/com/Main.java
+++ src/class_11_compundintrest/com/Main.java
@@ -1,4 +1,38 @@
 package class_11_compundintrest.com;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        double principal;
+        double rate;
+        int timesCompounded;
+        int years;
+        double amount;
+
+        System.out.print("Enter the principal amount: ");
+        principal = scanner.nextDouble();
+
+        System.out.print("Enter the interest rate (in %): ");
+        rate = scanner.nextDouble() / 100;
+
+        System.out.print("Enter the # of yimes compounded per year: ");
+        timesCompounded = scanner.nextInt();
+
+        System.out.print("Enter the number of years: ");
+        years = scanner.nextInt();
+
+        amount = principal * Math.pow(1 + rate / timesCompounded, timesCompounded * years);
+
+        System.out.printf("The amount after %d years is $%.2f", years, amount);
+
+
+
+
+        scanner.close();
+
+    }
 }
