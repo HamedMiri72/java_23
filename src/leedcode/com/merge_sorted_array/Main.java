ff029a774ade29a2bf9f271d556a8703af4fc2a2
HamedMiri72 <hamedmiri72@gmail.com>
2025-03-26 23:39:43 +0000

    fixing merge in leedcode

--- src/leedcode/com/merge_sorted_array/Main.java
+++ src/leedcode/com/merge_sorted_array/Main.java
@@ -1,4 +1,4 @@
-package merge_sorted_array.com;
+package leedcode.com.merge_sorted_array;
 
 import java.util.Arrays;
 
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args){
 
 
-        int[] num1 = {1,2,3,0,0,0};
+        int[] num1 = {7,8,9,0,0,0};
         int m = 3;
         int[] num2 = {2,5,6};
         int n = 3;
